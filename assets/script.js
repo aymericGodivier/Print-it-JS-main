fb8c9cbfbf82a81a705b45653ec7e0ed24423cd2
aymericGodivier <44438326+aymericGodivier@users.noreply.github.com>
2024-02-27 12:48:42 +0100

    codeJS

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,40 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let leftArrow = document.getElementById("arrow-left");
+let rightArrow = document.getElementById("arrow-right");
+let currentIndex = 1;
+let sliderText = document.getElementById("slider-text");
+let sliderImage = document.getElementsByClassName("banner-img")[0];
+
+function changeText(index){
+	let message = slides[index].tagLine;
+	sliderText.innerHTML = message;
+}
+
+function changeImage(index){
+	let imageSrc = slides[index].image;
+	let imagePath = "./assets/images/slideshow/" + imageSrc;
+	sliderImage.src = imagePath;
+}
+
+leftArrow.addEventListener("click", function() {
+	currentIndex--;
+	if(currentIndex<0){
+		currentIndex = slides.length-1;
+	}
+	changeImage(currentIndex);
+    changeText(currentIndex);
+});
+
+rightArrow.addEventListener("click", function() {
+	currentIndex++;
+	if(currentIndex>slides.length-1){
+		currentIndex = 0;
+	}
+	changeImage(currentIndex);
+    changeText(currentIndex);
+});
+
+
